70be4788859b9ac2e8c0b55a114033fb4217d992
harshmangalam <harshdev8218@gmail.com>
2024-09-12 11:15:39 +0530

    added linktree file

--- README.md
+++ README.md
@@ -13,3 +13,26 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.1.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+
+## Example
+
+```ts
+import { scrapeLinktreeAccount } from "./src";
+
+const { account, error, raw, statusCode } = await scrapeLinktreeAccount(
+  "harshmangalam_"
+);
+
+if (statusCode === 404) {
+  throw new Error("Oops! We couldn't find the username you're looking for");
+}
+if (error) {
+  console.error(error);
+}
+
+if (account) {
+  const { username, links, socialLinks, profilePictureUrl } = account;
+}
+
+```
---
+++ src/linktree.ts
@@ -0,0 +1,50 @@
+import * as cheerio from "cheerio";
+import { LINKTREE_BASE_URL, USER_NOT_FOUND } from "@/constants";
+import type { LinktreeResponse, ScrapeLinktreeProfile } from "@/types";
+
+export async function scrapeLinktreeAccount(
+  username: string
+): Promise<ScrapeLinktreeProfile> {
+  const initialData: ScrapeLinktreeProfile = {
+    account: null,
+    error: null,
+    raw: null,
+    statusCode: null,
+  };
+  const url = `${LINKTREE_BASE_URL}/${username}`;
+  try {
+    // Fetch the Linktree profile page
+    const resp = await fetch(url);
+    const data = await resp.text();
+
+    // Load the HTML into cheerio
+    const $ = cheerio.load(data);
+
+    const scriptContent = $("#__NEXT_DATA__").html();
+
+    if (scriptContent) {
+      // Parse the JSON content from the script tag
+      const jsonData = JSON.parse(scriptContent) as LinktreeResponse;
+      if (jsonData.props.pageProps.account) {
+        return {
+          ...initialData,
+          account: jsonData.props.pageProps.account,
+          raw: jsonData.props.pageProps,
+        };
+      }
+      return {
+        ...initialData,
+        error: USER_NOT_FOUND,
+      };
+    }
+    return {
+      ...initialData,
+      error: USER_NOT_FOUND,
+    };
+  } catch (error: any) {
+    return {
+      ...initialData,
+      error: USER_NOT_FOUND,
+    };
+  }
+}
